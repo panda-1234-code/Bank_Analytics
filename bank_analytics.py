93358cdeeefedafc71cc53b7ec18ea8801efd49f
panda-1234-code <pg23monali.bachhawat@isbmb.ac.in>
2024-09-19 13:45:48 +0530

    Hi This is my analysis on Bank analytics

--- bank_analytics.py
+++ bank_analytics.py
@@ -1,9 +1,10 @@
 import pandas as pd
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("input/bank.csv", sep=';')
+data = pd.read_csv("inputs/bank.csv", sep=';')
 
 print(data.head())
 
